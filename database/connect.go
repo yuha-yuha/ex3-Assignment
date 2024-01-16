4e8b984793ffca820c7c303b601b2669941f60c1
ishizu.shimabukuro <ishizushimadev@gmail.com>
2024-01-17 00:33:14 +0900

    migration

--- database/connect.go
+++ database/connect.go
@@ -1,15 +1,23 @@
 package database
 
 import (
+	"ej-ex3-backend/model"
+
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
+
+
 
 func DBconn() *gorm.DB {
 	db, err := gorm.Open(sqlite.Open("ex3"))
 	if err != nil {
 		panic("field to connect database")
 	}
+
+	db.AutoMigrate(&model.User{})
+	db.AutoMigrate(&model.UserWord{})
+	db.AutoMigrate(&model.DefaultWord{})
 
 	return db
 }
