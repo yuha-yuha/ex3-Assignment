a3504389d65eb343d487c34c02f1f99464484fbf
ishizu.shimabukuro <ishizushimadev@gmail.com>
2024-01-16 16:55:31 +0900

    loginできるようにした

--- controller/handler/login.go
+++ controller/handler/login.go
@@ -1,7 +1,11 @@
 package handler
 
 import (
+	"ej-ex3-backend/helper"
+	"ej-ex3-backend/model"
+
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func Login(ctx *gin.Context) {
@@ -10,11 +14,23 @@ func Login(ctx *gin.Context) {
 		Email    string `json:"email"`
 		Password string `json:"password"`
 	}
-	err := ctx.BindJSON(&RequestJson)
 
+	err := ctx.BindJSON(&RequestJson)
 	if err != nil {
 		ctx.Status(403)
 		ctx.Abort()
 	}
+
+	user := model.GetUser(RequestJson.Email)
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(RequestJson.Password))
+	if err != nil {
+		ctx.Status(403)
+		ctx.Abort()
+	}
+
+	token := helper.CreateJWT(user)
+
+	ctx.SetCookie("token", token, 3600, "/", "localhost", false, true)
 
 }
