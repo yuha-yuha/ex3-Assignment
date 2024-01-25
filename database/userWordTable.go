30a28618331f1877fc21c9148dc34eead8075d93
ishizu.shimabukuro <ishizushimadev@gmail.com>
2024-01-25 09:35:25 +0900

    もろもろ変更

--- database/userWordTable.go
+++ database/userWordTable.go
@@ -4,13 +4,13 @@ import (
 	"ej-ex3-backend/model"
 )
 
-func CreateWord(word model.UserWord) {
+func CreateWord(word model.UserWord, user model.User) {
 	db := DBconn()
 
 	db.Create(&word)
 }
 
-func GetALLUserWords() ([]model.UserWord, error) {
+func GetALLUsersWords() ([]model.UserWord, error) {
 	db := DBconn()
 
 	var uw []model.UserWord
@@ -18,4 +18,12 @@ func GetALLUserWords() ([]model.UserWord, error) {
 	err := db.Find(&uw).Error
 
 	return uw, err
+}
+
+func GetALLUserWords(uid int64) ([]model.UserWord, error) {
+	db := DBconn()
+	user := model.User{}
+	err := db.First(&user, uid).Error
+
+	return user.UserWords, err
 }
