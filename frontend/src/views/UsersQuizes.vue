c6a7df27a466f3eb90027bfb64cada0b21e304e9
ishizu.shimabukuro <ishizushimadev@gmail.com>
2024-01-29 14:30:29 +0900

    fix:

--- frontend/src/views/UsersQuizes.vue
+++ frontend/src/views/UsersQuizes.vue
@@ -5,8 +5,10 @@
     import { storeToRefs } from 'pinia';
     import type { popScopeId } from 'vue';
     import { onMounted } from 'vue';
+    import { useUserStore } from '@/stores/Request';
 
     const wordStore = useWordStore()
+    const userStore = useUserStore()
     const {wordList} = storeToRefs(wordStore)
 
     const UserAnswer = ref('')
@@ -19,13 +21,16 @@
         if (wordList.value.length !== 0) {
         const NewWord = wordList.value.shift() as Word
         word.value = NewWord
+
+        userStore.userFetch(word.value.user_id)
         }
     } 
 
     function JudgeAnswer() {
         if (UserAnswer.value === word.value?.english) {
             if (wordList.value.length === 0) {
-                //終了画面に移動
+                word.value.japanese = "終了！！！！ホームに戻れ！！！！"
+                word.value.english = ""
             } else {
                 PopWord()
                 
@@ -44,10 +49,18 @@
         justify="center" align-content="center">
         <v-col cols="6" lg="4" class="text-center">
                 <v-card color="grey-lighten-3" width="600px" height="350px" style="margin-top: 80px;" text-color="grey lighten-1">
-                    <v-card-text class="center-text text-h4 d-flex align-center justify-center">{{ word.japanese }}</v-card-text>
+                    <v-card-text class="center-text text-h4 d-flex align-center justify-center" style="white-space: pre-line;">
+                        <div style="margin-bottom: 20px;">{{ word.japanese }}</div>
+                    </v-card-text>
                 </v-card>
         </v-col>
 
+    </v-row>
+
+    <v-row>
+        <v-col class="text-center">
+            <div class="text-h4" v-if="word.english.length !== 0">作問者:{{userStore.user.name }}</div>
+        </v-col>
     </v-row>
 
     <v-row
